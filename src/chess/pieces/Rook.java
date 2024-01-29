49a79b1275a488bfd6d12a2286bc819f4bd7796f
HeDuz7 <notluiz901@gmail.com>
2024-01-29 14:58:55 -0300

    possible movies

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -5,11 +5,19 @@ import chess.ChessPiece;
 import chess.Color;
 
 public class Rook extends ChessPiece {
+
     public Rook(Board board, Color color) {
         super(board, color);
     }
-    @java.lang.Override
-    public java.lang.String toString(){
+
+    @Override
+    public String toString() {
         return "R";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+        return mat;
     }
 }
